17df849837d9f59a20ad71c5bfd0ca55105f7c46
Glitch (glitch-hello-website) <none>
2022-07-21 01:45:11 +0000

    ⏳🛋 Checkpoint ./script.js:45688779/507 ./index.html:45688779/12

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 This is your site JavaScript code - you can add interactivity and carry out processing
 - Initially the JS writes a message to the console, and moves a button you can add from the README
 */
-var AT_url = "https://api.airtable.com/v0/appSfzhC5ABBvTp2O/Table%201"
+var AT_url = "https://api.airtable.com/v0/appSfzhC5ABBvTp2O/RC_data"
 var AT_key = "api_key=keySsUNwZfQgXnsjZ"
-var SUMMARY_QUERY = "fields%5B%5D=Name%5B%5D=Photo%5B%5D=Information%5B%5D=Number%5B%5D=website%5B%5D=Desc%5B%5D=AcceptedDevices%5B%5D=methods%5B%5D=Appointment?";
+var SUMMARY_QUERY = "fields%5B%5D=Name&fields%5B%5D=Photo&fields%5B%5D=Information";
 
 function getBSC() {
-  var eWRC_Element = document.getElementById("name");
+  var eWRC_Element = document.getElementById("RC center");
 
-  fetch(`${AT_url}?${AT_key}&view=order&${SUMMARY_QUERY}`)
+  fetch(`${AT_url}?${AT_key}&${SUMMARY_QUERY}`)
     .then((response) => response.json())
     .then((data) => {
       console.log(data); // response is an object w/ .records array
@@ -20,10 +20,10 @@ function getBSC() {
 
       for (var i = 0; i < data.records.length; i++) {
         var name = data.records[i].fields["Name"];
-        var picture = data.records[i].fields["Photo"];
+        var photo = data.records[i].fields["Photo"];
         var info = data.records[i].fields["Information"];
       /*  var num = data.records[i].fields["Number"];
-        var link = data.records[i].fields["website"];
+        var link = data.records[i].fields["Website"];
         var desc = data.records[i].fields["Desc"];
         var aD = data.records[i].fields["AcceptedDevices"]
         var methods = data.records[i].fields["methods"];
@@ -35,13 +35,13 @@ function getBSC() {
           <div class="col-md-4 cardImageText">
           <div class="card">
             
-              >${picture ? `<img class="head" src="${picture[0].url}">` : ``}
+              >${photo ? `<img class="head" src="${photo[0].url}">` : ``}
             
 
             <div class="card-body">
               <p class="card-text card-key">${name}</p>
               <p class="card-text card-key">${info}</p>
-              <p class="card-text card-key">${desc}</p>
+             
             </div>
           </div>
         </div>
@@ -49,7 +49,14 @@ function getBSC() {
         `;
       }
 
-       eWRC_Element.innerHTML.innerHTML = newHtml;
+       eWRC_Element.innerHTML = newHtml;
     });
 }
 
+var idParams = window.location.search.split("?id=");
+if (idParams.length >= 2) {
+  // has at least ["id?", "OUR ID"]
+  getSingleDirectory(idParams[1]); // create detail view HTML w/ our id
+} else {
+  getBSC(); // no id given, fetch summaries
+}
